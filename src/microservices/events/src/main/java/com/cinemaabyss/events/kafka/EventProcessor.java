a9197a30a28c3e255128f2f34b5eaae0b0fe8ed0
akurus <akurus@tradingview.com>
2025-06-03 21:34:01 +0400

    3. Задание

--- src/microservices/events/src/main/java/com/cinemaabyss/events/kafka/EventProcessor.java
+++ src/microservices/events/src/main/java/com/cinemaabyss/events/kafka/EventProcessor.java
@@ -23,7 +23,7 @@ public class EventProcessor {
             logger.info("Обработка события фильма: id={}, movieId={}, action={}",
                     event.getId(), event.getMovieId(), event.getAction());
 
-            String topic = "movie-events";
+            String topic = "movie-events_proseed";
             producer.send(topic, event);
         } catch (IOException e) {
             logger.error("Ошибка обработки события фильма: {}", e.getMessage(), e);
@@ -36,7 +36,7 @@ public class EventProcessor {
             logger.info("Обработка события пользователя: id={}, userId={}, action={}",
                     event.getId(), event.getUserId(), event.getAction());
 
-            String topic = "user-events";
+            String topic = "user-events_proseed";
             producer.send(topic, event);
         } catch (IOException e) {
             logger.error("Ошибка обработки события пользователя: {}", e.getMessage(), e);
@@ -49,7 +49,7 @@ public class EventProcessor {
             logger.info("Обработка события платежа: id={}, paymentId={}, status={}",
                     event.getId(), event.getPaymentId(), event.getStatus());
 
-            String topic = "payment-events";
+            String topic = "payment-events_proseed";
             producer.send(topic, event);
         } catch (IOException e) {
             logger.error("Ошибка обработки события платежа: {}", e.getMessage(), e);
